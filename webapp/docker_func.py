9f215fb23b534131f33e93247f9101002e93786b
Maksim Pryanikov <makslight@gmail.com>
2019-10-10 18:55:28 +0300

    v3

--- webapp/docker_func.py
+++ webapp/docker_func.py
@@ -2,7 +2,8 @@ import docker
 
 client = docker.from_env()
 
-inf_b_treat = client.containers.list(all)
-for inf_w_treat in inf_b_treat:
-    int_temp=inf_b_treat['Container']
-    print(client.containers.get(int_temp).attrs['Name'])
+inf_b_treat = client.containers.list(all,id)
+
+#for inf_w_treat in inf_b_treat:
+#    int_temp=inf_b_treat['Container']
+#    print(client.containers.get(int_temp).attrs['Name'])
