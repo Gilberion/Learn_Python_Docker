8479b6d6ab3e2aa040df2a09ed35cee7216e53fd
Alex Glau <glaualex89@gmail.com>
2019-11-09 11:22:01 +0500

    Refactored containers control function

--- webapp/admin/views.py
+++ webapp/admin/views.py
@@ -9,12 +9,13 @@ from pywintypes import error as pywintypes_err
 
 blueprint = Blueprint('admin', __name__, url_prefix='/admin')
 
+
 @blueprint.route('/')
 @login_required
 def admin_index():
     cont_form = Toolbar()
     logout = Logout()
-    contadd =  Contadd()
+    contadd = Contadd()
 
     try:
         container_list = get_list()
@@ -39,34 +40,12 @@ def conteiner_proc():
     cont_id = request.form['container_id']
     cont_action = request.form['container_action']
 
-    if cont_action == 'start':
+    action_list = ['start', 'stop', 'kill', 'restart', 'pause', 'resume', 'remove']
+    if cont_action in action_list:
         container = client.containers.get(cont_id)
-        container.start()
+        getattr(container, cont_action)()
         return redirect(url_for('admin.admin_index'))
-    elif cont_action == 'stop':
-        container = client.containers.get(cont_id)
-        container.stop()
-        return redirect(url_for('admin.admin_index'))
-    elif cont_action == 'kill':
-        container = client.containers.get(cont_id)
-        container.kill()
-        return redirect(url_for('admin.admin_index'))
-    elif cont_action == 'restart':
-        container = client.containers.get(cont_id)
-        container.restart()
-        return redirect(url_for('admin.admin_index'))
-    elif cont_action == 'pause':
-        container = client.containers.get(cont_id)
-        container.pause()
-        return redirect(url_for('admin.admin_index'))
-    elif cont_action == 'resume':
-        container = client.containers.get(cont_id)
-        container.restart()
-        return redirect(url_for('admin.admin_index'))
-    elif cont_action == 'remove':
-        container = client.containers.get(cont_id)
-        container.remove()
-        return redirect(url_for('admin.admin_index'))
+
 
 @blueprint.route('/conteiner_add', methods=["POST"])
 @login_required
@@ -78,5 +57,3 @@ def conteiner_add():
     except docker.errors.ImageNotFound:
         flash('Такого контейнера не существует')
         return redirect(url_for('admin.admin_index'))
-
-        
